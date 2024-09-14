d30e05009905f1c4c171be59e7a049bc393ec557
Nicolae32 <nicolaestoicoi4@gmail.com>
2024-09-14 22:20:14 +0300

    a

--- main.cpp
+++ main.cpp
@@ -2,35 +2,30 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 
-
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1500, 1500), ":]");
     sf::RectangleShape player(sf::Vector2f(200, 200));
-    sf::Clock clock;  
+    sf::Clock clock;
     sf::Time time;
     sf::Event event;
     sf::Text text;
     sf::Texture player_texture;
-    player_texture.loadFromFile("player_texture.png")   ;
+    player_texture.loadFromFile("player_texture.png");
     player.setTexture(&player_texture);
     player.setOrigin(player.getSize().x / 2, player.getSize().y / 2);
 
-    while (window.isOpen())                            //The game loop.
+    while (window.isOpen()) // The game loop.
     {
-        
 
         time = clock.getElapsedTime();
 
-        std::cout << int(time.asSeconds()) << "\n";      //Gets time in seconds as integer.
-
-
-
+        std::cout << int(time.asSeconds()) << "\n"; // Gets time in seconds as integer.
 
         while (window.pollEvent(event))
         {
 
-            switch (event.type)                        //The debugging thing.
+            switch (event.type) // The debugging thing.
             {
 
             case sf::Event::Closed:
@@ -44,49 +39,40 @@ int main()
             case sf::Event::MouseWheelScrolled:
                 std::cout << "Wheel Scrolled" << "\n";
                 break;
-
             }
-
         }
 
-       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))       //Gets the Keyboard input.
-           {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) // Gets the Keyboard input.
+        {
 
-           player.move(-0.1f, 0.0f);
+            player.move(-0.1f, 0.0f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+        {
 
-           }
-       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-           {
+            player.move(0.0f, -0.1f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+        {
 
-           player.move(0.0f, -0.1f);
+            player.move(0.0f, 0.1f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+        {
 
-           }
-       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-           {
+            player.move(0.1f, 0.0f);
+        }
 
-           player.move(0.0f, 0.1f);
-
-           }
-       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-           {
-
-           player.move(0.1f, 0.0f);
-
-           }
-           
-
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))         //Moves the player to mouse.
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) // Moves the player to mouse.
         {
             sf::Vector2i mspoz = sf::Mouse::getPosition(window);
             player.setPosition(static_cast<float>(mspoz.x), static_cast<float>(mspoz.y));
-            
         }
-            
+
         window.clear();
         window.draw(player);
         window.display();
     }
 
-
-	return 0;
+    return 0;
 }
